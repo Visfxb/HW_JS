ed7483d8432b66f98cb123cd4b3653e13980f5ba
Visfxb <evervirus228@gmail.com>
2025-12-15 20:49:30 +0100

    HW-4.1

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log(`Hello world`);
+function CaesarСipher(str, k) {
+    output = ``;
+    for(let char of str) {
+        const isChar = /^[A-Z]+$/.test(char);
+        if (isChar) {
+            if (char.charCodeAt(0) - 'A'.charCodeAt(0) - k < 0)
+                output += String.fromCodePoint(char.charCodeAt(0) + 26 - k)
+            else
+                output += String.fromCodePoint(char.charCodeAt(0) - k)
+        }
+        else
+            output += char;
+    }
+    return output;
+}
+
+let str = prompt(`Enter string(A-Z)`)
+let k = +prompt(`Enter k`)
+alert(CaesarСipher(str, k))
